5ee4aba75af6637143e78555311adba2dffb2343
Asad Aftab <asadaftab64@gmail.com>
2021-02-15 15:09:27 +0500

    End Of section 2

---
+++ API/Data/Migrations/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Asad");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Ayesha");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "jibran"); 
